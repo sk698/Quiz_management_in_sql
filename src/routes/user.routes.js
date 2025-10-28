4fef6ce7c551837b6fb0c255324d98d5a023805f
sk698 <85735810+sk698@users.noreply.github.com>
2025-10-28 14:17:35 +0530

    Change changePassword route to POST method

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,7 @@ router.route("/login").post(loginUser);
 //secured user routes
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").get(verifyJWT,refreshAccessToken)
-router.route("/changePassword").get(verifyJWT,changeCurrentPassword)
+router.route("/changePassword").post(verifyJWT,changeCurrentPassword)
 
 
 
